b864324cd29b45d03cd9521f886da3a9444d8c13
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-07-30 20:14:28 +0300

    Fix base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  //base: "/happy-birthday/",
+  base: '/happy-birthday/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
